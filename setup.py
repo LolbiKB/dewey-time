d6d3ce6d2032468e356016ffc61cf9ae7e067d49
LolbiKB <lolbikb@gmail.com>
2026-05-28 11:52:03 +0700

    Refactor setup.py and pyproject.toml to specify package directory and update package finding location for zkteco_hr.

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     name="zkteco_hr",
     version="0.0.1",
     description="ZKTeco attendance engine (Frappe/ERPNext HRMS) - MVP flags + weekly view",
-    packages=find_packages(),
+    packages=find_packages(where="zkteco_hr"),
+    package_dir={"": "zkteco_hr"},
     include_package_data=True,
     zip_safe=False,
 )
